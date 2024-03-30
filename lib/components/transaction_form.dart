8ba53e4665cfb17efab4dcddd0f4af8151f4b7a7
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-03-30 15:49:11 -0300

    Numeric Keyboard

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -8,6 +8,17 @@ class TransactionForm extends StatelessWidget {
 
   final void Function(String, double) onSubmit;
 
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0.0;
+
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+
+    onSubmit(title, value);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -18,12 +29,15 @@ class TransactionForm extends StatelessWidget {
           children: [
             TextField(
               controller: titleController,
+              onSubmitted: (_) => _submitForm(),
               decoration: const InputDecoration(
                 labelText: "Título",
               ),
             ),
             TextField(
                 controller: valueController,
+                keyboardType: const TextInputType.numberWithOptions(),
+                onSubmitted: (_) => _submitForm(),
                 decoration: const InputDecoration(
                   labelText: "Valor (R\$)",
                 )),
@@ -31,17 +45,13 @@ class TransactionForm extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
                 TextButton(
+                  onPressed: _submitForm,
                   child: const Text(
                     'Nova Transação',
                     style: TextStyle(
                       color: Colors.purple,
                     ),
                   ),
-                  onPressed: () {
-                    final title = titleController.text;
-                    final value = double.tryParse(valueController.text) ?? 0.0;
-                    onSubmit(title, value);
-                  },
                 )
               ],
             ),
