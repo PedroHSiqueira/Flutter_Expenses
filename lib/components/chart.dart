a5b7f2d33be3495f74d069e10783957654438a68
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-04-02 18:20:21 -0300

    feat: Create of Chart.dart file

---
+++ lib/components/chart.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_expenses/models/transaction.dart';
+
+class Chart extends StatelessWidget {
+  const Chart(this.recentTrasaction, {super.key});
+
+  final List<Transaction> recentTrasaction;
+
+  List<Map<String, Object>> get groupedTrasaction {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recentTrasaction.length; i++) {
+        bool sameDay = recentTrasaction[i].date.day == weekDay.day;
+        bool sameMonth = recentTrasaction[i].date.month == weekDay.month;
+        bool sameYear = recentTrasaction[i].date.year == weekDay.year;
+
+        if(sameDay && sameMonth && sameYear){
+          totalSum += recentTrasaction[i].value;
+        }
+      }
+
+      return {
+        "day": DateFormat.E().format(weekDay)[0],
+        "value": totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
+    );
+  }
+}
