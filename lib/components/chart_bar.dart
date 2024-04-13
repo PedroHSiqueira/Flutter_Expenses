74ffd35891b7cfa49190be33cc69246be410034f
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-04-13 13:37:45 -0300

    🔨 Feat: Add Automatic heights using Builder Method

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class ChartBar extends StatelessWidget {
   const ChartBar({
@@ -15,48 +14,51 @@ class ChartBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          height: 20,
-          child: FittedBox(
-            child: Text(value.toStringAsFixed(2)),
+    return LayoutBuilder(builder: (ctx, constrains) {
+      return Column(
+        children: [
+          SizedBox(
+            height: constrains.maxHeight * 0.15,
+            child: FittedBox(
+              child: Text(value.toStringAsFixed(2)),
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        SizedBox(
-          height: 60,
-          width: 10,
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1.0,
-                    ),
-                    color: const Color.fromRGBO(220, 220, 220, 1),
-                    borderRadius: BorderRadius.circular(5)),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentage,
-                child: Container(
+          SizedBox(height: constrains.maxHeight * 0.05),
+          SizedBox(
+            height: constrains.maxHeight * 0.6,
+            width: 10,
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      color: const Color.fromRGBO(220, 220, 220, 1),
                       borderRadius: BorderRadius.circular(5)),
                 ),
-              ),
-            ],
+                FractionallySizedBox(
+                  heightFactor: percentage,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(5)),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        const SizedBox(
-          height: 5,
-        ),
-        Text(label),
-      ],
-    );
+          SizedBox(
+            height: constrains.maxHeight * 0.05,
+          ),
+          Container(
+            height: constrains.maxHeight * 0.15,
+            child: FittedBox(child: Text(label)),
+          ),
+        ],
+      );
+    });
   }
 }
