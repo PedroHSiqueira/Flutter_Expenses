a2d155ad0a0d7fddd92384150c1366fec99d5cac
PedroHSiqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2024-04-16 13:20:05 -0300

    🔧 Fix: Use contraints to change the height on Transaction List

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,22 +11,27 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return transactions.isEmpty
-        ? Column(
-            children: [
-              const SizedBox(height: 20),
-              Text(
-                "Nenhuma Transação Registrada!",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                height: 200,
-                child: Image.asset(
-                  "assets/images/waiting.png",
-                  fit: BoxFit.cover,
-                ),
-              )
-            ],
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: [
+                  SizedBox(height: constraints.maxHeight * 0.05),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.3,
+                    child: Text("Nenhuma Transação Registrada!",
+                        style: Theme.of(context).textTheme.titleLarge),
+                  ),
+                  SizedBox(height: constraints.maxHeight * 0.3),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      "assets/images/waiting.png",
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            },
           )
         : ListView.builder(
             itemCount: transactions.length,
@@ -34,19 +39,18 @@ class TransactionList extends StatelessWidget {
               final tr = transactions[index];
               return Card(
                 elevation: 5,
-                margin:
-                    const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
                 child: ListTile(
                   leading: CircleAvatar(
                     radius: 30,
                     child: Padding(
                       padding: const EdgeInsets.all(6),
-                      child: FittedBox(child: Text("R\$${tr.value}")),
+                      child: FittedBox(child: Text("R\$${tr.value}"))
                     ),
                   ),
                   title: Text(
                     tr.title,
-                    style: Theme.of(context).textTheme.titleSmall,
+                    style: Theme.of(context).textTheme.titleSmall
                   ),
                   subtitle: Text(DateFormat("d MMM y").format(tr.date)),
                   trailing: IconButton(
